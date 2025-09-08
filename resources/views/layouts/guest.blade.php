a49108a4aac0f6503cdb74fafdd43bce19c2efb9
Akhmad Yusuf <afpentech@gmail.com>
2025-09-08 16:01:38 +0800

    logo web

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -16,11 +16,15 @@
     </head>
     <body class="font-sans text-gray-900 antialiased">
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
-            <div>
-                <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-                </a>
-            </div>
+            <img src="{{ asset('assets/icons/logo.svg') }}"
+            alt="Logo"
+            class="w-24 h-24 max-w-full flex-shrink-0 dark:filter dark:brightness-0 dark:invert mb-3">
+            <div class="flex items-center justify-start mb-3 transition-all duration-300">
+           
+           <div class="menu-text ml-3 text-md text-3xl font-bold text-gray-800 dark:text-gray-200">
+               Welcome
+           </div>
+       </div>
 
             <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
                 {{ $slot }}
