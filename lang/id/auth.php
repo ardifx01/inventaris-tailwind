e96d32015c130062b936fad4e98d7c756c320c7c
Akhmad Yusuf <afpentech@gmail.com>
2025-09-09 10:20:30 +0800

    laravel indonesia localization

---
+++ lang/id/auth.php
@@ -0,0 +1,8 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'failed'   => 'Email atau Password Salah.',
+    'throttle' => 'Terlalu banyak upaya masuk. Silahkan coba lagi dalam :seconds detik.',
+];
